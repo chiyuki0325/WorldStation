ebdf7845e246df4b25cd4513aee161a31c21fbdf
Kirikaze Chiyuki <me@chyk.ink>
2025-05-31 14:32:36 +0800

    [new folder] Vee-you-eee

---
+++ web/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ScrollingBackground from "./components/ScrollingBackground.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/favicon.png" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <ScrollingBackground />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ web/src/components/ScrollingBackground.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {useDark} from '@vueuse/core'
+
+const isDark = useDark()
+</script>
+
+<template>
+  <div class="scrolling-background">
+    <div class="background-image" v-for="i in 10" :key="i">
+      <img src="/background-dark.png" alt="背景图" v-if="isDark"/>
+      <img src="/background-light.png" alt="背景图" v-else/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.scrolling-background {
+  overflow-x: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  height: 100vh;
+  width: max-content;
+  animation: scrollBackground 60s linear infinite;
+  display: flex;
+  flex-direction: row;
+}
+
+.background-image > img {
+  height: 100vh;
+  -webkit-user-drag: none;
+  user-drag: none;
+  -webkit-user-select: none;
+  user-select: none;
+  pointer-events: none;
+}
+
+
+@keyframes scrollBackground {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
+</style>
