cbd910f21043eee65c0df30ace2b1f3941b1d941
Kirikaze Chiyuki <me@chyk.ink>
2025-08-18 17:23:43 +0800

    [improvement] 404

--- web/src/views/NotFoundView.vue
+++ web/src/views/NotFoundView.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>404 Not Found</h1>
-  <p>您访问的页面不存在。</p>
-  <p>单击<a href="/">这里</a>返回首页。</p>
+  <Semisolid color="white">
+    <p>您访问的页面不存在！</p>
+    <p>点击<a href="/">这里</a>返回首页。</p>
+  </Semisolid>
 </template>
+
+<script setup>
+import Semisolid from "../components/Semisolid.vue";
+</script>
